0aad7fdaddb733c3b73d7704c28901409642ad4c
stevematdavies <stevematdavies@gmail.com>
2019-01-02 20:11:42 +0200

    adding quamtity to products data

--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -4,17 +4,20 @@ export const ProductData = [
     name: 'Circle',
     description: 'Perfect choice when you don’t need any corners.',
     price: 999,
+    quantity: 1
   },
   {
     id: 2,
     name: 'Square',
     description: 'For once, it’s a great idea to think inside the box.',
-    price: 899
+    price: 899,
+    quantity: 1
   },
   {
     id: 3,
     name: 'Triangle',
     description: 'A true classic with three elegant corners.',
-    price: 1009
+    price: 1009,
+    quantity: 1
   }
 ];
