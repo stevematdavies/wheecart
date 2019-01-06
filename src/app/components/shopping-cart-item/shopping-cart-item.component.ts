8813c63cab764ae06b2d90e05a85da905f65384e
stevematdavies <stevematdavies@gmail.com>
2019-01-06 19:25:03 +0200

    cleaned up shoppping-cart-item component

--- src/app/components/shopping-cart-item/shopping-cart-item.component.ts
+++ src/app/components/shopping-cart-item/shopping-cart-item.component.ts
@@ -1,4 +1,3 @@
-import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
@@ -8,25 +7,17 @@ import { Product } from '../product/Product';
 @Component({
   selector: 'app-shopping-cart-item',
   templateUrl: './shopping-cart-item.component.html',
-  styleUrls: ['./shopping-cart-item.component.scss']
-})
+  styleUrls: ['./shopping-cart-item.component.scss']})
 export class ShoppingCartItemComponent implements OnInit {
 
-  @Input() item: Product;
-  mobileResponseBreakPointReached = false;
+  @Input()item: Product;
 
   constructor(
     private shoppingCartService: ShoppingCartService,
     private route: ActivatedRoute,
-    private router: Router,
-    public breakpointObserver: BreakpointObserver) {}
+    private router: Router) {}
 
   ngOnInit() {
-    this.breakpointObserver
-    .observe(['(max-width: 800px)'])
-        .subscribe((state: BreakpointState) => {
-          this.mobileResponseBreakPointReached = state.matches;
-        });
   }
 
   onRemoveFromCart(item: Product) {
@@ -34,7 +25,7 @@ export class ShoppingCartItemComponent implements OnInit {
   }
 
   navigateToInfoView(id: number) {
-    this.router.navigate([`item/c/${id}`], { relativeTo: this.route });
+    this.router.navigate([`item/c/${id}`], {relativeTo: this.route});
   }
 
 }
